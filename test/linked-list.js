001bcc241347579627e0ec33ad28918665f936de
Marcus Pöhls <mp@seda.digital>
2021-12-22 10:04:06 +0100

    add tests

--- test/linked-list.js
+++ test/linked-list.js
@@ -18,4 +18,22 @@ test('push', () => {
   expect(LinkedList.from([1]).push(2).toArray()).toEqual([1, 2])
 })
 
+test('isEmpty', () => {
+  expect(LinkedList.from().isEmpty()).toBe(true)
+  expect(LinkedList.from([]).isEmpty()).toBe(true)
+
+  expect(LinkedList.from([1]).isEmpty()).toBe(false)
+  expect(LinkedList.from([]).push(1).isEmpty()).toBe(false)
+  expect(LinkedList.from([]).push(null).isEmpty()).toBe(false)
+})
+
+test('isNotEmpty', () => {
+  expect(LinkedList.from().isNotEmpty()).toBe(false)
+  expect(LinkedList.from([]).isNotEmpty()).toBe(false)
+
+  expect(LinkedList.from([1]).isNotEmpty()).toBe(true)
+  expect(LinkedList.from([]).push(1).isNotEmpty()).toBe(true)
+  expect(LinkedList.from([]).push(null).isNotEmpty()).toBe(true)
+})
+
 test.run()
