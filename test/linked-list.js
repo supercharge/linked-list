8e5f40ae93b53181ab95d6f66cd5b6c1fad39359
Marcus Pöhls <mp@seda.digital>
2021-12-23 10:06:51 +0100

    add tests

--- test/linked-list.js
+++ test/linked-list.js
@@ -36,4 +36,65 @@ test('isNotEmpty', () => {
   expect(LinkedList.from([]).push(null).isNotEmpty()).toBe(true)
 })
 
+test('head', () => {
+  expect(LinkedList.from().head()).toBeUndefined()
+  expect(LinkedList.from([]).head()).toBeUndefined()
+
+  expect(LinkedList.from(1).head().value()).toBe(1)
+  expect(LinkedList.from(1, 2).head().value()).toBe(1)
+
+  expect(LinkedList.from([1]).head().value()).toBe(1)
+  expect(LinkedList.from([1, 2]).head().value()).toBe(1)
+})
+
+test('tail', () => {
+  expect(LinkedList.from().tail()).toBeUndefined()
+  expect(LinkedList.from([]).tail()).toBeUndefined()
+
+  expect(LinkedList.from(1).tail().value()).toBe(1)
+  expect(LinkedList.from(1, 2).tail().value()).toBe(2)
+  expect(LinkedList.from(1, 2, 3).tail().value()).toBe(3)
+
+  expect(LinkedList.from([1]).tail().value()).toBe(1)
+  expect(LinkedList.from([1, 2]).tail().value()).toBe(2)
+  expect(LinkedList.from([1, 2, 3]).tail().value()).toBe(3)
+})
+
+test('toArray', () => {
+  const users = [
+    { id: 1, name: 'Marcus' },
+    { id: 2, name: 'Supercharge' }
+  ]
+
+  const list = LinkedList.from(users)
+  expect(list.toArray()).toEqual(users)
+})
+
+test('nodes', () => {
+  const users = [
+    { id: 1, name: 'Marcus' },
+    { id: 2, name: 'Supercharge' }
+  ]
+
+  const list = LinkedList.from(users)
+  expect(list.nodes().length).toBe(2)
+  expect(list.nodes()[0].value()).toEqual({ id: 1, name: 'Marcus' })
+  expect(list.nodes()[1].value()).toEqual({ id: 2, name: 'Supercharge' })
+})
+
+test('find', () => {
+  const list = LinkedList.from([
+    { id: 1, name: 'Marcus' },
+    { id: 2, name: 'Supercharge' }
+  ])
+
+  expect(
+    list.find(node => node.value().name === 'Marcus').value()
+  ).toEqual({ id: 1, name: 'Marcus' })
+
+  expect(
+    list.find(value => value.name === 'Supercharge')
+  ).toBeUndefined()
+})
+
 test.run()
